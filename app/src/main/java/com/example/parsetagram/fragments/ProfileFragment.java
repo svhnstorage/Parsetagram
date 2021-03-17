592062a3f7d7a607a9e956bb5af6093e0890eb8f
svhnstorage <svhnstorage@gmail.com>
2021-03-17 14:17:45 -0400

    second load

--- app/src/main/java/com/example/parsetagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/parsetagram/fragments/ProfileFragment.java
@@ -1,4 +1,38 @@
 package com.example.parsetagram.fragments;
 
-public class ProfileFragment {
+import android.util.Log;
+
+import com.example.parsetagram.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ProfileFragment extends PostFragment{
+    @Override
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
+
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if( e!= null){
+                    Log.e(TAG, "issue with getting posts", e);
+                    return;
+                }
+                for(Post post : posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+
+                }
+                adapter.clear();
+                adapter.addAll(posts);
+                swipeContainer.setRefreshing(false);
+            }
+        });    }
 }
